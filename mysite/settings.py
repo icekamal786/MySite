8e145c90a924bc3f5f642d45ed99c55e2cc14369
icekamal786 <116745248+icekamal786@users.noreply.github.com>
2022-11-05 18:57:16 +0530

    2w

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static')
+   os.path.join(BASE_DIR, 'mysite/static')
 ]
 django_heroku.settings(locals())
 
